299fd080c4148a84658cd8d5261477cabf2ebf66
kopy011 <kopy.kovacs@gmail.com>
2022-09-25 14:43:19 +0200

    client initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <v-app-bar>
+        <v-app-bar-title>
+            Home
+        </v-app-bar-title>
+    </v-app-bar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'App',
+        data() {
+            return {
+
+            }
+        }
+    });
+</script>
