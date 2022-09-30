75077a68c3d2fc56257870de5cfe143847169ea4
kopy011 <kopy.kovacs@gmail.com>
2022-09-30 16:14:02 +0200

    fix for routing to non existant path

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,32 +1,41 @@
+import { nextTick } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/HomeView.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/map',
+    name: 'map',
+    component: () => import('../views/Map.vue')
+  },
+  {
+    path: '/locations',
+    name: 'locations',
+    component: () => import('../views/Locations.vue')
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/map',
-      name: 'map',
-      component: () => import('../views/Map.vue')
-    },
-    {
-      path: '/locations',
-      name: 'locations',
-      component: () => import('../views/Locations.vue')
-    }
-  ]
+  routes: routes
+});
+
+router.beforeEach((to, from) => {
+  if(!routes.some((route) => route.path === to.path)){
+    return { name: 'home' };
+  }
 })
 
 export default router
