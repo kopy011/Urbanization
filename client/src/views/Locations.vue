8114fb6537d8c95ffa6782765dff6e17e60f50ff
kopy011 <kopy.kovacs@gmail.com>
2022-10-01 18:56:36 +0200

    edit form ui

--- client/src/views/Locations.vue
+++ client/src/views/Locations.vue
@@ -29,6 +29,23 @@
             </tbody>
           </v-table>
         </v-col>
+        <v-spacer/>
+        <v-col cols="3">
+          <v-form v-model="valid">
+            <v-container>
+              <v-text-field label="Name" prepend-inner-icon="mdi-tag-outline" :rules="textRules"/>
+              <v-text-field label="Lat" prepend-inner-icon ="mdi-counter" type="number" :rules="numberRules"/>
+              <v-text-field label="Lon" prepend-inner-icon ="mdi-counter" type="number" :rules="numberRules"/>
+              <v-text-field label="Diagonal" prepend-inner-icon ="mdi-arrow-left-right-bold-outline" type="number" :rules="numberRules"/>
+              <v-row class="mt-5">
+                <v-btn color="info">Import</v-btn>
+                <v-spacer />
+                <v-btn color="error">Reset</v-btn>
+                <v-btn color="success" class="ml-3">Save</v-btn>
+              </v-row>
+            </v-container>
+          </v-form>
+        </v-col>
       </v-row>
     </v-container>
 </template>
@@ -43,6 +60,14 @@
 
     data() {
         return{
+          valid: false as boolean,
+          textRules: [
+            (v: string) => !!v || 'Field is requiered!'
+          ],
+          numberRules: [
+            (v: number) => !!v || 'Field is requiered!',
+            (v: number) => v >= 0 || 'Only positive numbers are accepted!'
+          ]
         }
     },
 
